3d6726e8adb904736c23b46d3b698cf6c7ccbc8b
jani-nykanen <jnyknn@gmail.com>
2019-02-14 23:09:42 +0200

    Fixed file extensions

--- dev/convert.sh
+++ dev/convert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
-../tools/png2bin bitmaps/font.png ../ASSETS/BITMAPS/FONT.bin
-../tools/png2bin bitmaps/player.png ../ASSETS/BITMAPS/PLAYER.bin
+../tools/png2bin bitmaps/font.png ../ASSETS/BITMAPS/FONT.BIN
+../tools/png2bin bitmaps/player.png ../ASSETS/BITMAPS/PLAYER.BIN
