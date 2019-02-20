ed2ec0fbdeaa80f739ff0988d4d1d3cfb07f686a
jani-nykanen <jnyknn@gmail.com>
2019-02-20 09:47:42 +0200

    Moar tiles

---
+++ dev/maps/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tileset" tilewidth="16" tileheight="16" tilecount="33" columns="11">
+ <image source="../bitmaps/tileset.png" width="176" height="48"/>
+</tileset>
