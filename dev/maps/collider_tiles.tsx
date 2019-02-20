947d32289438cad3dacdd1b5983238b1f70e22df
jani-nykanen <jnyknn@gmail.com>
2019-02-20 23:00:11 +0200

    Even moar tiles

---
+++ dev/maps/collider_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="collider_tiles" tilewidth="16" tileheight="16" tilecount="16" columns="16">
+ <image source="collider_tiles.png" width="256" height="16"/>
+</tileset>
